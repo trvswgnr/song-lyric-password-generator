94eb826b8789d5e6cd33286743cb2aeaf7bd345f
trav <trvswgnr@gmail.com>
2019-02-07 21:13:39 -0500

    set ssl:verify-certificate no

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ abort() {
 [ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
 [ -z $FTP_USER ] && abort "FTP_USER is undefined"
 [ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
-lftp -c "open -u $FTP_USER,$FTP_PASS ftp.travisawagner.com; set ssl:verify-certificate no; mirror -R ~/ /travisaw.com/test-production/synth-patch-name-generator"
+lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
+  set ssl:verify-certificate no \
+  -e 'mirror -c -e -R ~/ /travisaw.com/test-production/synth-patch-name-generator ; exit'
