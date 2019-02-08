ccafc9f79bfee6d853ca3adab338a435c73d386e
trav <trvswgnr@gmail.com>
2019-02-07 21:05:42 -0500

    update

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 abort() {
-    local message=$1
-    echo $message
-    exit -1
+  local message=$1
+  echo $message
+  exit -1
 }
 [ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
 [ -z $FTP_USER ] && abort "FTP_USER is undefined"
 [ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
-lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
- -e 'mirror -c -e -R ~/ /travisaw.com/test-production/synth-patch-name-generator ; exit'
+lftp -c "open -u $FTP_USER,$FTP_PASS ftp.travisawagner.com; set ssl:verify-certificate no; mirror -R ~/ /travisaw.com/test-production/synth-patch-name-generator"
