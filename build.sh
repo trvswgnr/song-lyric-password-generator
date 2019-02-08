2a8a0a466fb375e1ee071546bf28b292833bfb43
trav <trvswgnr@gmail.com>
2019-02-07 21:47:29 -0500

    this is it

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
-abort() {
-  local message=$1
-  echo $message
-  exit -1
-}
-[ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
-[ -z $FTP_USER ] && abort "FTP_USER is undefined"
-[ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
-lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
-  -e 'set ssl:verify-certificate no; mirror -c -e -R ./ /travisaw.com/test-production/synth-patch-name-generator ; exit'
+#!/usr/bin/env bash
+set -e
+
+LOCALPATH='./'
+REMOTEPATH='/travisaw.com/test/song-lyrics-password-generator'
+
+lftp -f "
+set dns:order 'inet'
+open ftp://$FTP_SITE
+user $FTP_USER $FTP_PASS
+set ssl:verify-certificate no
+mirror --continue --reverse --delete $LOCALPATH $REMOTEPATH
+bye
+"
