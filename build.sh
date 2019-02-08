d81fb4190dbe84a81d760ff2ac1536c6b10f9e06
trav <trvswgnr@gmail.com>
2019-02-07 21:55:17 -0500

    change

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ REMOTEPATH='/travisaw.com/test/song-lyrics-password-generator'
 
 lftp -f "
 set dns:order 'inet'
-open ftp://$FTP_SITE
-user $FTP_USER $FTP_PASS
+open -u $FTP_USER,$FTP_PASS $FTP_SITE
 set ssl:verify-certificate no
 mirror -R $LOCALPATH $REMOTEPATH
 bye
