576d96db7928aa046a71030fc78229daa35e3da7
trav <trvswgnr@gmail.com>
2019-02-07 22:04:10 -0500

    please god

--- build.sh
+++ build.sh
@@ -4,10 +4,4 @@ set -e
 LOCALPATH='./'
 REMOTEPATH='/travisaw.com/test/song-lyrics-password-generator'
 
-lftp -f "
-set dns:order 'inet'
-open -u $FTP_USER,$FTP_PASS $FTP_SITE
-set ssl:verify-certificate no
-mirror -R $LOCALPATH $REMOTEPATH
-bye
-"
+lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_SITE; set ssl:verify-certificate no; mirror -R $LOCALPATH $REMOTEPATH"
