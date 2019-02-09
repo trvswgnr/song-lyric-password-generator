1e9728a5014b3ebf8d5ca9c8356b21c4b1bdd302
trav <trvswgnr@gmail.com>
2019-02-09 00:04:45 -0500

    cant mkdir bc permissions, try --relative rsync instead

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 set -e
 
 deployment() {
-  DEPLOY_PATH="./travisaw.com/test/dev/song-lyrics-password-generator"
-  mkdir -p $DEPLOY_PATH
-  rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:$DEPLOY_PATH
+  DEPLOY_PATH="/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator"
+  rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ -a --relative $DEPLOY_PATH travisaw@travisawagner.com:/
 }
 
 deployment
