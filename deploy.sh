af0851149ca69cbf53118923e80f7f34d50d851d
trav <trvswgnr@gmail.com>
2019-02-08 23:55:45 -0500

    no permission, so use current path

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 deployment() {
-  DEPLOY_PATH="/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator"
+  DEPLOY_PATH="./travisaw.com/test/dev/song-lyrics-password-generator"
   mkdir -p $DEPLOY_PATH
   rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:$DEPLOY_PATH
 }
