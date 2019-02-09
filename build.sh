65658b0200c516a6cf8fa158ced9ac417102b536
trav <trvswgnr@gmail.com>
2019-02-08 22:48:06 -0500

    rm

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-LOCALPATH='./'
-REMOTEPATH='/travisaw.com/test/song-lyrics-password-generator'
-
-lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_SITE; set ssl:verify-certificate no; mirror -R $LOCALPATH $REMOTEPATH"
