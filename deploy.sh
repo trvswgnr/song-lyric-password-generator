829e2dc93121b2a7fac4c13829a419777ed09ff5
trav <trvswgnr@gmail.com>
2019-02-09 00:31:51 -0500

    add exclude from file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-deployment() {
+deploy() {
   DEPLOY_PATH="/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator"
-  rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:$DEPLOY_PATH
+  rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ --exclude-from='./.deployignore' travisaw@travisawagner.com:$DEPLOY_PATH
 }
 
-deployment
+deploy
