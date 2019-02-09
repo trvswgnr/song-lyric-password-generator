fba856d48b57e6247a1d24402ee8844481fb3448
trav <trvswgnr@gmail.com>
2019-02-08 23:37:32 -0500

    actually deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-echo $TRAVIS_BUILD_DIR $TRAVIS_BRANCH
-exit 0
-
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:/home/travisaw/travisaw.com/test/song-lyrics-password-generator
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator
