6d20a55bac0c49a5227b0228480bf0f2454bb827
trav <trvswgnr@gmail.com>
2019-02-08 23:53:01 -0500

    move into function

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator
+deployment() {
+  DEPLOY_PATH="/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator"
+  mkdir -p $DEPLOY_PATH
+  rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:$DEPLOY_PATH
+}
+
+deployment
