25f43d948123d2658b084d3ff23172b51b68bb46
trav <trvswgnr@gmail.com>
2019-02-09 00:17:36 -0500

    try again with directory already created on server

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ set -e
 
 deployment() {
   DEPLOY_PATH="/home/travisaw/travisaw.com/test/dev/song-lyrics-password-generator"
-  mkdir -p ~/travisaw.com/test/dev/song-lyrics-password-generator
   rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ travisaw@travisawagner.com:$DEPLOY_PATH
 }
 
